71e66b56b4c6f5d7b00ab0cfc712392b87a88b3e
Michael Naquin <mnaquin1@yahoo.com>
2020-09-15 16:25:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -122,6 +122,10 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
+   if ([LEFT_ARROW, RIGHT_ARROW].indexOf(e.which) - 1) {
+     e.stopPropagation()
+     e.preventDefault()
+   }
 }
 
 function moveDodgerLeft() {
